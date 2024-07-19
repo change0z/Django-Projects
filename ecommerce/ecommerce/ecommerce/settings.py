1e1c98360264f99b3fe8a104fe51c6e196b24972
change0z <shaikh.ziyad.mr@gmail.com>
2024-07-19 01:55:05 +0100

    changed the allowed hosts

--- ecommerce/ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-*g@pvb8xg=d#eq$4ntc=wwun*y)hi7eaqeill^27z4y13gltyb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
